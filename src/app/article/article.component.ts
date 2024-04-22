7eb8fc60ab5d0ffd2cf4a5565bb59c55a7ac4299
Felipe Ainie <discoedy@gmail.com>
2024-04-22 11:48:13 -0400

    Se Crea component article,utilizamos @HostBinding

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,26 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  title: string;
+  votes: number;
+  link: string;
+  constructor() {
+    this.title = 'Angular';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  voteUp() {
+    this.votes += 1;
+  }
+  voteDown() {
+    this.votes -= 1;
+  }
+  ngOnInit(): void {}
+}
